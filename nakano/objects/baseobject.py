b5d5f6612782ff99075ff51e4429eda3aa0e2b99
Okimii <robertomc2017@gmail.com>
2022-06-13 20:27:42 -0400

    made the BaseObject more descriptive

--- nakano/objects/baseobject.py
+++ nakano/objects/baseobject.py
@@ -3,4 +3,4 @@ __all__ = ["BaseObject"]
 class BaseObject:
 
     def __repr__(self) -> str:
-        return f"ID: {self.id}, OBJECT_TYPE: {self.__class__.__name__}"
+        return f"<OBJECT_TYPE={self.__class__.__name__} ID={self.id}>"
