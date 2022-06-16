f17981282bec9e900ef1a5e0e261b7fac753a995
Okimii <robertomc2017@gmail.com>
2022-06-16 02:31:12 -0400

    added some handy stuff

--- nakano/objects/baseobject.py
+++ nakano/objects/baseobject.py
@@ -1,6 +1,17 @@
 __all__ = ["BaseObject"]
 
+
+
+from typing import ClassVar
+import datetime
+
+
 class BaseObject:
+
+    id: ClassVar[int] = 0
 
     def __repr__(self) -> str:
         return f"<OBJECT_TYPE={self.__class__.__name__} ID={self.id}>"
+    
+    def snowflake_time(self) -> datetime.datetime:
+        return datetime.datetime.fromtimestamp(timestamp = ((self.id >> 22) +  1420070400000) / 1000, tz=datetime.timezone.utc)
--- nakano/objects/snowflake.py
+++ nakano/objects/snowflake.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 
+from .baseobject import BaseObject
+
+
 __all__ = ["SnowFlake"]
 
 @dataclass
-class SnowFlake:
+class SnowFlake(BaseObject):
 
     id: int
